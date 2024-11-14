e400efb4c199825994a2f36ef3abc40580ab1f2a
Maxime Gros <mxs.grs@gmail.com>
2024-11-14 11:58:14 +0100

    Material

--- src/app/task-manager/task-manager-wrapper/task-manager-wrapper.component.ts
+++ src/app/task-manager/task-manager-wrapper/task-manager-wrapper.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TaskManagerFormComponent } from "../task-manager-form/task-manager-form.component";
+import { TaskManagerListComponent } from '../task-manager-list/task-manager-list.component';
 
 @Component({
   selector: 'app-task-manager-wrapper',
   standalone: true,
-  imports: [],
+  imports: [TaskManagerFormComponent, TaskManagerListComponent],
   templateUrl: './task-manager-wrapper.component.html',
   styleUrl: './task-manager-wrapper.component.css'
 })
