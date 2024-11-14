57795676bc60de656cf5c077deda2bb251101812
Maxime Gros <mxs.grs@gmail.com>
2024-11-14 22:43:43 +0100

    Reactive form

--- src/app/features/tasks/components/task-form/task-form.component.ts
+++ src/app/features/tasks/components/task-form/task-form.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatOption } from '@angular/material/core';
 import { MatFormField, MatLabel} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButton } from '@angular/material/button';
 import { TaskStatus } from '../../models/task-status';
+import { TaskService } from '../../services/task.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [MatFormField, MatInput, MatOption, MatLabel, MatSelectModule, MatButton],
+  imports: [MatFormField, MatInput, MatOption, MatLabel, MatSelectModule, MatButton, ReactiveFormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
+  form: FormGroup;
+  taskService: TaskService = inject(TaskService);
   taskStatuses = Object.values(TaskStatus);
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id: [1],
+      name: [''],
+      description: [''],
+      status: [TaskStatus.Pending],
+      creationDate: [new Date()]
+    })
+  }
+
+  onSubmit() {
+    console.log('submit');
+    this.taskService.addTask(this.form.value);
+  }
 }
