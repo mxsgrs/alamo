aa73d9ca510d8b3da167c769f420dd8c113935d6
Maxime Gros <mxs.grs@gmail.com>
2024-11-15 21:41:45 +0100

    Form validation and snack bar

--- src/app/features/tasks/components/task-form/task-form.component.ts
+++ src/app/features/tasks/components/task-form/task-form.component.ts
@@ -4,9 +4,10 @@ import { MatFormField, MatLabel} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButton } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { TaskStatus } from '../../models/task-status';
 import { TaskService } from '../../services/task.service';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
@@ -19,12 +20,13 @@ export class TaskFormComponent {
   form: FormGroup;
   taskService: TaskService = inject(TaskService);
   taskStatuses = Object.values(TaskStatus);
+  private _snackBar = inject(MatSnackBar);
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       id: [1],
-      name: [''],
-      description: [''],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
       status: [TaskStatus.Pending],
       creationDate: [new Date()]
     })
@@ -33,5 +35,8 @@ export class TaskFormComponent {
   onSubmit() {
     console.log('submit');
     this.taskService.addTask(this.form.value);
+    this._snackBar.open('Your task was added to the list!', 'Ok', {
+      duration: 3000
+    });
   }
 }
